23f4eb7caa5290798ed3d3476566c7853081d1b5
Rafael Correa <rc443@njit.edu>
2017-06-30 18:25:25 -0400

    created a database.php file to log into the server

---
+++ database.php
@@ -0,0 +1,27 @@
+<?php
+	$dsn = 'mysql:dbname=rc443;host=sql12.njit.edu';
+	$username = 'rc443';
+	$password = 'Gpsc539F';
+
+	try{
+		$db = new PDO($dsn, $username, $password);
+		$dbh->setAttribute(PDO;:ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch (PDOException $e) {
+		echo 'Connection failed: ' . $e->getMessage();
+	}
+$sql = "CREATE TABLE users1 (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50),
+	reg_date TIMESTAMP
+	)";
+try {
+
+$dbh->exec($sql);
+} catch (PDOException $e) {
+echo $sql . "<br>" . $e->getMessage();
+
+}
+echo 'done';
+?>
